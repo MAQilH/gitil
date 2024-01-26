47e0ba7c3f3bfa5e1e899e788b6b76405a14c656
AQil <mohammada.hafezi@gmail.com>
2024-01-26 20:21:54 +0330

    sec comit

--- gitil.c
+++ gitil.c
@@ -3,6 +3,6 @@
 
 int main(int argc, char* argv[]){
     printf("num: %d\n%s + %s\n", argc, argv[1], argv[2]);
-    int a = 3, b = 82334;
+    int a = 3, b = 8233434;
     printf("%d", sum(a, b));
 }
